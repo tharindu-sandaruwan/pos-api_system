11b4bc8f12357efb6dbc78c240e2abbb2e9955e5
tharindu-sandaruwan <tharindusandaruwan011@gmail.com>
2025-03-31 21:13:41 +0530

    Done some Entity classes

--- src/main/java/com/example/pos_api/model/Admin.java
+++ src/main/java/com/example/pos_api/model/Admin.java
@@ -8,16 +8,11 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Product {
+public class Admin {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String name;
-    private String description;
-    private Double price;
-    private String imageUrl;
-    private String stock;
-    private String category;
+    private boolean permission;
 
 }
--- src/main/java/com/example/pos_api/model/ShoppingCart.java
+++ src/main/java/com/example/pos_api/model/ShoppingCart.java
@@ -8,16 +8,12 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Product {
+public class ShoppingCart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String name;
-    private String description;
-    private Double price;
-    private String imageUrl;
-    private String stock;
-    private String category;
+    private Long id;
+    private Integer quantity;
+
 
 }
