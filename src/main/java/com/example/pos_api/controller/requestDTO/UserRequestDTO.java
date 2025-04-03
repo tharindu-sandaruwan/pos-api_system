4e0a2e54480d5843587449750e33c4156ef6c289
tharindu-sandaruwan <tharindusandaruwan011@gmail.com>
2025-04-03 19:59:32 +0530

    done user create part

--- src/main/java/com/example/pos_api/controller/requestDTO/UserRequestDTO.java
+++ src/main/java/com/example/pos_api/controller/requestDTO/UserRequestDTO.java
@@ -1,4 +1,18 @@
 package com.example.pos_api.controller.requestDTO;
 
+import com.example.pos_api.model.UserRole;
+import lombok.Data;
+
+@Data
 public class UserRequestDTO {
+
+    private String userName;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private UserRole role;
+
+
 }
--- src/main/java/com/example/pos_api/service/UserService.java
+++ src/main/java/com/example/pos_api/service/UserService.java
@@ -1,4 +1,9 @@
 package com.example.pos_api.service;
 
+import com.example.pos_api.controller.requestDTO.UserRequestDTO;
+
 public interface UserService {
+
+    void createUser(UserRequestDTO userRequestDTO);
+
 }
